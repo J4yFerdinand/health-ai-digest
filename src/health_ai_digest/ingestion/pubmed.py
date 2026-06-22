b88f3a62cca91882c7f8c567aa441ad704aab5b4
J4yFerdinand <fernandotech1498@gmail.com>
2026-06-22 18:57:45 -0500

    feat: implement PubMed metadata fetch

--- src/health_ai_digest/ingestion/pubmed.py
+++ src/health_ai_digest/ingestion/pubmed.py
@@ -5,14 +5,20 @@ from health_ai_digest.models.article import Article
 
 class PubMedClient(BaseIngestionClient):
   SEARCH_URL = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi"
+  FETCH_URL = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi"
 
   def fetch(self, query: str, limit: int = 10) -> list[Article]:
+    # Search PubMed and fetch full metadata for the resulting articles.
     pmids = self._search(query, limit)
-    print(pmids)
+    xml_data = self._fetch_details(pmids)
+
+    # Temporal: Manual Inspection of XML
+    print(xml_data[:1000])
 
     return []
   
   def _search(self, query: str, limit: int) -> list[str]:
+    # Search PubMed and return a list of PMIDs.
     params = {
       "db": "pubmed",
       "term": query,
@@ -26,3 +32,22 @@ class PubMedClient(BaseIngestionClient):
     data = response.json()
 
     return data["esearchresult"]["idlist"]
+  
+  def _fetch_details(self, pmids: list[str]) -> str:
+    """
+    Fetch article metadata from PubMed using PMIDs.
+    Returns raw XML response.
+    """
+    if not pmids:
+      return ""
+    
+    params = {
+      "db": "pubmed",
+      "id": ",".join(pmids),
+      "retmode": "xml"
+    }
+
+    response = requests.get(self.FETCH_URL, params=params)
+    response.raise_for_status()
+
+    return response.text
