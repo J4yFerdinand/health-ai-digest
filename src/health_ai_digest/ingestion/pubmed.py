8df52ccc0a40ff26828815743b0a2b31dee444a6
J4yFerdinand <fernandotech1498@gmail.com>
2026-06-22 14:47:02 -0500

    refactor: extract PubMed search logic into private method

--- src/health_ai_digest/ingestion/pubmed.py
+++ src/health_ai_digest/ingestion/pubmed.py
@@ -4,9 +4,15 @@ from health_ai_digest.ingestion.base import BaseIngestionClient
 from health_ai_digest.models.article import Article
 
 class PubMedClient(BaseIngestionClient):
-  BASE_URL = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi"
+  SEARCH_URL = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi"
 
   def fetch(self, query: str, limit: int = 10) -> list[Article]:
+    pmids = self._search(query, limit)
+    print(pmids)
+
+    return []
+  
+  def _search(self, query: str, limit: int) -> list[str]:
     params = {
       "db": "pubmed",
       "term": query,
@@ -14,10 +20,9 @@ class PubMedClient(BaseIngestionClient):
       "retmode": "json",
     }
 
-    response = requests.get(self.BASE_URL, params=params)
+    response = requests.get(self.SEARCH_URL, params=params)
     response.raise_for_status()
 
     data = response.json()
-    print(data)
 
-    return []
+    return data["esearchresult"]["idlist"]
