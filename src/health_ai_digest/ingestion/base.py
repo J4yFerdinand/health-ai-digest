53d83e71f0961edb3e9951afde81f02655f70ff3
J4yFerdinand <fernandotech1498@gmail.com>
2026-06-22 12:43:50 -0500

    refactor: improve ingestion client interface with result limit

--- src/health_ai_digest/ingestion/base.py
+++ src/health_ai_digest/ingestion/base.py
@@ -4,5 +4,8 @@ from health_ai_digest.models.article import Article
 class BaseIngestionClient(ABC):
 
   @abstractmethod
-  def fetch(self, query: str) -> list[Article]:
+  def fetch(self, query: str, limit: int = 10) -> list[Article]:
+    """
+    Fetch articles from a source based on a search query.
+    """
     pass
