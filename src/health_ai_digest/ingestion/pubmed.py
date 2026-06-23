5dd646d612e6cf05fb27d201aa89c6205ba5842f
J4yFerdinand <fernandotech1498@gmail.com>
2026-06-22 19:55:40 -0500

    feat: parse PubMed XML into article objects

--- src/health_ai_digest/ingestion/pubmed.py
+++ src/health_ai_digest/ingestion/pubmed.py
@@ -1,7 +1,9 @@
 import requests
+import xml.etree.ElementTree as ET
 
 from health_ai_digest.ingestion.base import BaseIngestionClient
 from health_ai_digest.models.article import Article
+from health_ai_digest.models.enums import SourceType
 
 class PubMedClient(BaseIngestionClient):
   SEARCH_URL = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi"
@@ -11,11 +13,9 @@ class PubMedClient(BaseIngestionClient):
     # Search PubMed and fetch full metadata for the resulting articles.
     pmids = self._search(query, limit)
     xml_data = self._fetch_details(pmids)
+    articles = self._parse_articles(xml_data)
 
-    # Temporal: Manual Inspection of XML
-    print(xml_data[:1000])
-
-    return []
+    return articles
   
   def _search(self, query: str, limit: int) -> list[str]:
     # Search PubMed and return a list of PMIDs.
@@ -51,3 +51,33 @@ class PubMedClient(BaseIngestionClient):
     response.raise_for_status()
 
     return response.text
+  
+  def _parse_articles(self, xml_data: str) -> list[Article]:
+    if not xml_data:
+      return []
+    
+    root = ET.fromstring(xml_data)
+    articles = []
+
+    for pubmed_article in root.findall(".//PubmedArticle"):
+      article = self._parse_article(pubmed_article)
+      articles.append(article)
+
+    return articles
+  
+  def _parse_article(self, pubmed_article: ET.Element) -> Article:
+    # Parse a single PubMed article.
+    pmid = pubmed_article.findtext(".//PMID", default="")
+    title = pubmed_article.findtext(".//ArticleTitle", default="Untitled")
+
+    url = f"https://pubmed.ncbi.nlm.nih.gov/{pmid}/"
+
+    return Article(
+      title=title,
+      source=SourceType.PUBMED,
+      url=url,
+      abstract=None,
+      published_at=None,
+      keywords=[],
+      doi=None,
+    )
